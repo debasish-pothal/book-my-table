a7bd59482eb01b6ce2df0c02768233bcf0149fe3
Debasish Pothal <debasish_pothal@hotmail.com>
2023-04-19 15:44:00 +0530

    refactored the ui with new layout

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,22 +1,11 @@
-import NavBar from "@/app/components/NavBar";
-import Link from "next/link";
-import Header from "../components/Header";
 import RestaurantNavbar from "../components/RestaurantNavbar";
 import Menu from "../components/Menu";
 
 export default function RestaurantMenu() {
   return (
-    <main className="bg-white min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[100%] rounded p-3 shadow">
-            <RestaurantNavbar />
-            <Menu />
-          </div>
-        </div>
-      </main>
-    </main>
+    <div className="bg-white w-[100%] rounded p-3 shadow">
+      <RestaurantNavbar />
+      <Menu />
+    </div>
   );
 }
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,24 +1,17 @@
-import Link from "next/link";
-import NavBar from "../components/NavBar";
 import Header from "../components/Header";
 import SearchSidebar from "./components/SearchSidebar";
 import RestaurantCard from "./components/RestaurantCard";
 
 export default function Search() {
   return (
-    <main className="bg-white min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <main>
-          <Header />
-          <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-            <SearchSidebar />
-            <div className="w-5/6">
-              <RestaurantCard />
-            </div>
-          </div>
-        </main>
-      </main>
+    <main>
+      <Header />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <SearchSidebar />
+        <div className="w-5/6">
+          <RestaurantCard />
+        </div>
+      </div>
     </main>
   );
 }
